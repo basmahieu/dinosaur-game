7cca6b75df30388f8a408b4183b0d2407c59d8db
Bas Mahieu <bas.mahieu@gmail.com>
2020-12-01 11:22:00 +0100

    adding jump and moving cactus

--- scripts.js
+++ scripts.js
@@ -1,24 +1,58 @@
-const jump = document.querySelector('#dino');
+const dino = document.querySelector('#dino');
+const audio = document.querySelector('#audio');
+const cactus = document.querySelector('#cactus');
 
-// document.body.keydown = function(e){
-//     if(e.keyCode == 32){
-//         //your code
 
-//         jump.classList.add('jump');
+function jump() {
 
-        
+    if (dino.classList != "jump"){
+        dino.classList.add('jump');
+        audio.play();   
 
-//     }
-// }
-function test (){
-window.addEventListener('keydown', function(e) {
-    if(e.keyCode == 32){
-        e.preventDefault();
-        jump.classList.add('jump');
-        console.log('jumped');
-
+        setTimeout(function(){
+            dino.classList.remove('jump');
+        }, 300)
     }
+}   
+
+
+let isAlive = setInterval(() => {
+    // get current dino Y position
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+    // get current cactus X position
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+ 
+    // Detect collision
+    if (cactusLeft <50 && cactusLeft > 0 && dinoTop >= 140){
+    // alert ("You lose!");
+    } 
+        
+    
+
+
+}, 10);
+
+
+document.addEventListener('keydown', function(e) {
+    if(e.keyCode == 32){
+        jump();
+    }
+    
 });
+
+
+// Score
+const score = document.querySelector('.score');
+let seconds = 0;
+
+function stopWatch(){
+    let finalScore = seconds++;
+    score.innerHTML = `<h2>Score: ${finalScore} </h2>`;
 }
 
-test();
+window.setInterval(stopWatch, 100);
+
+
+
+
